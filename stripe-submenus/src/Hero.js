af885c590257c040bf81f2dd1378897220c28da0
masooma <masooma.zahedi1@gmil.com>
2021-09-13 18:27:43 -0500

    complete hero part

--- stripe-submenus/src/Hero.js
+++ stripe-submenus/src/Hero.js
@@ -2,9 +2,21 @@ import React from 'react'
 import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  return <section className="hero">
+    <div className="hero-center">
+    <article className="hero-info">
+      <h1>
+        Payments infrastructure for the internet
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vel officiis quia voluptatibus molestiae aliquid dolorum reiciendis, quos sed error numquam corrupti necessitatibus!</p>
+      <button className="btn">Start Now</button>
+    </article>
+    <article className="hero-images">
+      <img src={phoneImg} alt="phoneimage" />
+    </article>
+    </div>
+  </section>
 }
 
-export default Hero
+export default Hero;
